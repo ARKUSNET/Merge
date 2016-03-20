3f26b892b471ae8cdcd7af091d08c4cc3407c06e
Artur Kuspakov <artur-6272@rambler.ru>
2016-03-20 23:53:47 +0300

    first commit

---
+++ Merge.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Merge
+{
+    partial class Merge
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFirstFile = new System.Windows.Forms.Button();
+            this.btnSecondFile = new System.Windows.Forms.Button();
+            this.btnMerge = new System.Windows.Forms.Button();
+            this.textBoxFirstFullPath = new System.Windows.Forms.TextBox();
+            this.textBoxSecondFullPath = new System.Windows.Forms.TextBox();
+            this.richTextBoxMerge = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnFirstFile
+            // 
+            this.btnFirstFile.Location = new System.Drawing.Point(0, 11);
+            this.btnFirstFile.Name = "btnFirstFile";
+            this.btnFirstFile.Size = new System.Drawing.Size(113, 23);
+            this.btnFirstFile.TabIndex = 0;
+            this.btnFirstFile.Text = "Первый файл";
+            this.btnFirstFile.UseVisualStyleBackColor = true;
+            this.btnFirstFile.Click += new System.EventHandler(this.btnFirstFile_Click);
+            // 
+            // btnSecondFile
+            // 
+            this.btnSecondFile.Location = new System.Drawing.Point(0, 40);
+            this.btnSecondFile.Name = "btnSecondFile";
+            this.btnSecondFile.Size = new System.Drawing.Size(113, 23);
+            this.btnSecondFile.TabIndex = 1;
+            this.btnSecondFile.Text = "Второй файл";
+            this.btnSecondFile.UseVisualStyleBackColor = true;
+            this.btnSecondFile.Click += new System.EventHandler(this.btnSecondFile_Click);
+            // 
+            // btnMerge
+            // 
+            this.btnMerge.Location = new System.Drawing.Point(521, 14);
+            this.btnMerge.Name = "btnMerge";
+            this.btnMerge.Size = new System.Drawing.Size(106, 49);
+            this.btnMerge.TabIndex = 2;
+            this.btnMerge.Text = "Объединить";
+            this.btnMerge.UseVisualStyleBackColor = true;
+            this.btnMerge.Click += new System.EventHandler(this.btnMerge_Click);
+            // 
+            // textBoxFirstFullPath
+            // 
+            this.textBoxFirstFullPath.Location = new System.Drawing.Point(119, 14);
+            this.textBoxFirstFullPath.Name = "textBoxFirstFullPath";
+            this.textBoxFirstFullPath.Size = new System.Drawing.Size(379, 20);
+            this.textBoxFirstFullPath.TabIndex = 3;
+            // 
+            // textBoxSecondFullPath
+            // 
+            this.textBoxSecondFullPath.Location = new System.Drawing.Point(119, 43);
+            this.textBoxSecondFullPath.Name = "textBoxSecondFullPath";
+            this.textBoxSecondFullPath.Size = new System.Drawing.Size(379, 20);
+            this.textBoxSecondFullPath.TabIndex = 4;
+            // 
+            // richTextBoxMerge
+            // 
+            this.richTextBoxMerge.Location = new System.Drawing.Point(0, 70);
+            this.richTextBoxMerge.Name = "richTextBoxMerge";
+            this.richTextBoxMerge.Size = new System.Drawing.Size(627, 489);
+            this.richTextBoxMerge.TabIndex = 6;
+            this.richTextBoxMerge.Text = "";
+            // 
+            // Merge
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 561);
+            this.Controls.Add(this.richTextBoxMerge);
+            this.Controls.Add(this.textBoxSecondFullPath);
+            this.Controls.Add(this.textBoxFirstFullPath);
+            this.Controls.Add(this.btnMerge);
+            this.Controls.Add(this.btnSecondFile);
+            this.Controls.Add(this.btnFirstFile);
+            this.Name = "Merge";
+            this.Text = "Объединение файлов";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFirstFile;
+        private System.Windows.Forms.Button btnSecondFile;
+        private System.Windows.Forms.Button btnMerge;
+        private System.Windows.Forms.TextBox textBoxFirstFullPath;
+        private System.Windows.Forms.TextBox textBoxSecondFullPath;
+        private System.Windows.Forms.RichTextBox richTextBoxMerge;
+    }
+}
+
---
+++ Merge.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Merge
+{
+    public partial class Merge : Form
+    {
+        #region Variables
+
+        Dictionary<int, string> firstFile = new Dictionary<int, string>();
+        Dictionary<int, string> secondFile = new Dictionary<int, string>();
+        List<string> splitLineFirstFile = new List<string>();
+        List<string> splitLineSecondFile = new List<string>();
+        int index = 0;
+
+        #endregion
+        public Merge()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFirstFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialogChoiceFile((sender as Button).Text);
+        }
+
+        private void btnSecondFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialogChoiceFile((sender as Button).Text);
+        }
+
+        private void btnMerge_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(richTextBoxMerge.Text)) richTextBoxMerge.Clear();
+            if (!String.IsNullOrEmpty(textBoxFirstFullPath.Text) && !String.IsNullOrEmpty(textBoxSecondFullPath.Text))
+            {
+                string[] filePaths = { textBoxFirstFullPath.Text, textBoxSecondFullPath.Text };
+                ReadFileAndAddToDictionary(filePaths);
+                MergeBothFiles();
+                firstFile.Clear();
+                secondFile.Clear();
+            }
+        }
+
+        #region Methods
+
+        private void MergeBothFiles()
+        {
+            int indexMaxCount = firstFile.Count > secondFile.Count ? firstFile.Count : secondFile.Count;
+            int indexMinCount = firstFile.Count < secondFile.Count ? firstFile.Count : secondFile.Count;
+            for (int ii = 0; ii < indexMaxCount - 1; ii++)
+            {
+                string lineFromFirstFile = firstFile[ii];
+                if (ii > indexMinCount) return;
+                string lineFromSecondFile = secondFile[ii];
+                if (lineFromFirstFile.Equals(lineFromSecondFile))
+                {
+                    richTextBoxMerge.Text += lineFromFirstFile + "\n";
+                }
+                else
+                {
+                    MergeHelper(lineFromFirstFile, lineFromSecondFile);
+                }
+            }
+        }
+
+        private void MergeHelper(string lineFromFirstFile, string lineFromSecondFile)
+        {
+            string pattern = @"\w(?<!\d)[\w'-]*";            
+            int indexWord = 0;
+            foreach (Match m in Regex.Matches(lineFromFirstFile, pattern))
+            {
+                if(!String.IsNullOrEmpty(m.Value))
+                {
+                    splitLineFirstFile.Add(m.Value);
+                    indexWord++;
+                }
+            }
+            indexWord = 0;
+            foreach (Match m in Regex.Matches(lineFromFirstFile, pattern))
+            {
+                if (!String.IsNullOrEmpty(m.Value))
+                {
+                    splitLineSecondFile.Add(m.Value);
+                    indexWord++;
+                }
+            }
+        }
+
+        private void CheckAnyChangesInBothLine()
+        {
+
+        }
+
+        private void ReadFileAndAddToDictionary(string[] filePaths)
+        {
+            string readLine = String.Empty;
+            try
+            {
+                for (int ii = 0; ii < filePaths.Length; ii++)
+                {
+                    using (StreamReader sr = new StreamReader(filePaths[ii]))
+                    {
+                        while ((readLine = sr.ReadLine()) != null)
+                        {
+                            if (String.IsNullOrEmpty(readLine)) continue;
+                            if (ii == 0)
+                            {
+                                firstFile.Add(index, readLine.Trim());
+                            }
+                            else
+                            {
+                                secondFile.Add(index, readLine.Trim());
+                            }
+                            index++;
+                        }
+                        index = 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(String.Format("Файл не может быть прочитан: {0}", ex.Message), "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void OpenFileDialogChoiceFile(string btnText)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Выберите файл.";
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "txt файл (*.txt)|*.txt";
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if (btnText == "Первый файл")
+                    {
+                        textBoxFirstFullPath.Text = openFileDialog.FileName;
+                    }
+                    if (btnText == "Второй файл")
+                    {
+                        textBoxSecondFullPath.Text = openFileDialog.FileName;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка: Не могу прочитать файл с диска. Подробно: " + ex.Message);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
